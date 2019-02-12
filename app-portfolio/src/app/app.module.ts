5621fed864f339d54510509969fe6874b947e623
Jose Daniel <josedanielromero@hotmail.com>
2019-02-12 13:01:41 +0100

    me estoy volviendo loco

--- app-portfolio/src/app/app.module.ts
+++ app-portfolio/src/app/app.module.ts
@@ -4,28 +4,22 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 import { ListasDePortfoliosComponent } from './listas-de-portfolios/listas-de-portfolios.component';
-=======
-=======
+
+
 import { HeaderComponent } from './header/header.component';
->>>>>>> 2edef62af42122a51a6a02439fdae603d36f8b9f
+
 import { FooterComponent } from './footer/footer.component';
->>>>>>> f06efceccec111094db07d2e1642c52ebf98b048
+
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
-<<<<<<< HEAD
-    ListasDePortfoliosComponent
-=======
-=======
-    HeaderComponent
->>>>>>> 2edef62af42122a51a6a02439fdae603d36f8b9f
+    ListasDePortfoliosComponent,
+    HeaderComponent,
     FooterComponent
->>>>>>> f06efceccec111094db07d2e1642c52ebf98b048
+
   ],
   imports: [
     BrowserModule,
