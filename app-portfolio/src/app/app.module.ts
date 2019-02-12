8450748fe6236e6cb11d1167fa8ff709114836f6
Jorge Pampliega <jorge.pp.91@gmail.com>
2019-02-12 13:04:22 +0100

    se ve bien: parte de jorge, paula y JD

--- app-portfolio/src/app/app.module.ts
+++ app-portfolio/src/app/app.module.ts
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-
-import { AppComponent } from './app.component';
-
-import { ListasDePortfoliosComponent } from './listas-de-portfolios/listas-de-portfolios.component';
-
-
-import { HeaderComponent } from './header/header.component';
-
-import { FooterComponent } from './footer/footer.component';
-
-=======
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
@@ -22,27 +6,14 @@ import { AppComponent } from "./app.component";
 import { ListasDePortfoliosComponent } from "./listas-de-portfolios/listas-de-portfolios.component";
 import { HeaderComponent } from "./header/header.component";
 import { FooterComponent } from "./footer/footer.component";
->>>>>>> d208b0045a8054babe1210e1e136e3c56057bacf
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
-    ListasDePortfoliosComponent,
-    HeaderComponent,
-    FooterComponent
-
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-=======
     HeaderComponent,
     ListasDePortfoliosComponent,
     HeaderComponent,
     FooterComponent
->>>>>>> d208b0045a8054babe1210e1e136e3c56057bacf
   ],
   imports: [BrowserModule, FormsModule, HttpModule],
   providers: [],
