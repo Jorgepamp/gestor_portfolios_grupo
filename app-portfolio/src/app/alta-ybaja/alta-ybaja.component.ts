4fd0aad1669dea5f2003ad206e15d1b1ecc380af
Jorge Pampliega <jorge.pp.91@gmail.com>
2019-02-13 14:19:34 +0100

    añadido alta tambien

--- app-portfolio/src/app/alta-ybaja/alta-ybaja.component.ts
+++ app-portfolio/src/app/alta-ybaja/alta-ybaja.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { CambioMenuService } from "app/cambio-menu.service";
 
 @Component({
   selector: "app-alta-ybaja",
@@ -6,7 +7,49 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./alta-ybaja.component.css"]
 })
 export class AltaYBajaComponent implements OnInit {
-  constructor() {}
+  titulo: string;
 
-  ngOnInit() {}
+  etiqueta_nombre: string;
+  etiqueta_descripcion: string;
+  etiqueta_archivo: string;
+
+  placeholder_nombre: string;
+  placeholder_descripcion: string;
+  placeholder_archivo: string;
+
+  nombreIntroducido: string;
+  descripcionIntroducido: string;
+  archivoIntroducido: string;
+
+  servPortfolio: CambioMenuService;
+
+  constructor(sp: CambioMenuService) {
+    this.servPortfolio = sp;
+  }
+
+  ngOnInit() {
+    // Método correspondiente al evento al iniicializar el componente
+    this.titulo = "Alta de portfolios";
+    this.etiqueta_nombre = "Nombre y Apellidos";
+    this.placeholder_nombre = "Introduzca Nombre y Apellidos";
+    // this.nombreIntroducido = "Voldemort";
+    this.etiqueta_descripcion = "Descripción";
+    this.placeholder_descripcion = "Introduzca una descripción";
+    this.etiqueta_archivo = "Archivo";
+    this.placeholder_archivo = "Introduzca un archivo";
+  }
+  limpiar() {
+    this.nombreIntroducido = "";
+    this.descripcionIntroducido = "";
+    this.archivoIntroducido = "";
+    console.log("<<< Se limpia los campos >>>");
+  }
+  darAlta() {
+    this.servPortfolio.alta(
+      this.nombreIntroducido,
+      this.descripcionIntroducido,
+      this.archivoIntroducido
+    );
+    console.log("<<< Está dado de alta >>>");
+  }
 }
